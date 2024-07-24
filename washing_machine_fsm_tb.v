8a73ebacaa3ce31c880cec85bb1474235a10ff02
Ziad Abdelfattah <ziadahmed.a.elfattah@gmail.com>
2024-07-24 19:48:36 +0300

    Update washing_machine_fsm_tb.v

--- washing_machine_fsm_tb.v
+++ washing_machine_fsm_tb.v
@@ -5,23 +5,23 @@ module washing_machine_fsm_tb();
 ////////////////////// DUT Signals & Parameters //////////////////////
 
 
-reg				RST, CLK;
-reg				ON_SUMMER, ON_WINTER; 
-reg				TIMER_DONE; 		
-reg				DOOR_SENSOR, WATER_SENSOR_M, WATER_SENSOR_H;	
+reg		RST, CLK;
+reg		ON_SUMMER, ON_WINTER; 
+reg		TIMER_DONE; 		
+reg		DOOR_SENSOR, WATER_SENSOR_M, WATER_SENSOR_H;	
 
 wire	[1:0]	TIMER_SEL;										
-wire			TIMER_EN, WASHER_EN, WATER_EN, SPIN_EN, DRAIN_EN;
-wire			FILLING, ACTIVE, DRAIN, SPIN, IDLE, DONE;      
+wire		TIMER_EN, WASHER_EN, WATER_EN, SPIN_EN, DRAIN_EN;
+wire		FILLING, ACTIVE, DRAIN, SPIN, IDLE, DONE;      
 
 parameter	CLK_Period = 20,
-			idle_op    = 'b111,
-            fill_med   = 'b110,
-			fill_high  = 'b101,
-            activate   = 'b100,
-            drain      = 'b000,
-            spin_op    = 'b001,
-            end_op     = 'b011;
+		idle_op    = 'b111,
+                fill_med   = 'b110,
+		fill_high  = 'b101,
+            	activate   = 'b100,
+            	drain      = 'b000,
+            	spin_op    = 'b001,
+            	end_op     = 'b011;
 
 ////////////////////// Initial Block //////////////////////
 
@@ -184,10 +184,10 @@ end
 
 task initialize;
 begin
-	RST				= 'b1;
-	CLK				= 'b1;
-	ON_SUMMER		= 'b0;
-	ON_WINTER	    = 'b0;
+	RST		= 'b1;
+	CLK		= 'b1;
+	ON_SUMMER	= 'b0;
+	ON_WINTER	= 'b0;
 	TIMER_DONE      = 'b0;
 	DOOR_SENSOR     = 'b0;
 	WATER_SENSOR_M  = 'b0;
